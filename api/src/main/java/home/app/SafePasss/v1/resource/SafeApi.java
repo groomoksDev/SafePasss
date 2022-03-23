d8410eb3159c2d31bbc830d3e85bda6b5bbc51a5
groomok <groomoksdev@gmail.com>
2022-03-22 17:11:00 -0700

    Add swagger

--- api/src/main/java/home/app/SafePasss/v1/resource/SafeApi.java
+++ api/src/main/java/home/app/SafePasss/v1/resource/SafeApi.java
@@ -1,5 +1,6 @@
 package home.app.SafePasss.v1.resource;
 
+import io.swagger.annotations.ApiOperation;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -8,6 +9,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("safe-api")
 public class SafeApi {
+    @ApiOperation("some")
     @GetMapping("/hello")
     public String hello(@RequestParam(value = "name", defaultValue = "World") String name) {
         return String.format("Hello %s!", name);
